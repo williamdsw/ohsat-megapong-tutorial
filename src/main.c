c2495d335e5f46ca0ab01700c7537fa1227f1445
williamdsw <williamdsw.outlook.com>
2020-02-16 22:30:26 -0300

    Palettes

--- src/main.c
+++ src/main.c
@@ -7,11 +7,23 @@ int main ()
     // 1 = posicao do tile na VRAM
     VDP_loadTileSet (bgtile.tileset, 1, DMA);
 
-    // PLAN_A = foreground, como chao etc.
-    // PLAN_B = background, etc.
-    // tile = 8x8 pixels
-    // logo (1,1) = 8,8
-    VDP_setTileMapXY (PLAN_B, 1, 2, 2);
+    // Extrai paleta de cores do 'bgtile', e adiciona ao 'PAL1'
+    VDP_setPalette (PAL1, bgtile.palette -> data);
+
+    // Exibe tileset no PLAN_A na posicao (2,2)
+    // TILE_ATTR_FULL (...)
+    // - PAL1 = paleta a ser usada pelo tile
+    // - prio = permite sobreescrever prioridade de desenhar o tile (PLAN_A, PLAN_2)
+    // - flipV = inverte o tile verticalmente
+    // - flipX = inverte o tile horizontalmente
+    // - index = indice do tile na VRAM
+    VDP_setTileMapXY (PLAN_A, TILE_ATTR_FULL (PAL1, 0, FALSE, FALSE, 1), 2, 2);
+
+    // Desenha uma mapa de tiles retangular aonde:
+    // x, y = pontos de partida
+    // w, h = dimensoes do retangulo
+    VDP_fillTileMapRect (PLAN_B, TILE_ATTR_FULL (PAL1, 0, FALSE, FALSE, 1), 0, 0, 40, 30);
+
 
     // Escreve texto na tela
     //VDP_drawText ("Ola Mundo do Mega Drive no Windows!", 4, 12);
